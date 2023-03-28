3f29a9f0746e3b84438eea84dd1f52bc1ab0c842
Daniel Lafrance <roaddan@gmail.com>
2023-03-28 09:49:22 -0400

    20230328-094922

--- githubsave.sh
+++ githubsave.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-git status
-git add .
-git commit -m $(/bin/date "+%Y%m%d-%H%M%S")
-git push -u origin main
+export unedate="$(/bin/date "+%Y%m%d-%H%M%S")"
+case $1 in
+	status)
+		echo ${unedate}
+		git status
+		;;
+	*)
+		git add .
+		git commit -m $(/bin/date "+%Y%m%d-%H%M%S")
+		git push -u origin main
+		;;
+esac
+
