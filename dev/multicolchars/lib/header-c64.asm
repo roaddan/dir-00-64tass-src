6d8f02e5a230f00c92aa915f0739c482762a0737
Daniel Lafrance <roaddan@gmail.com>
2023-07-01 21:55:11 -0400

    20230701-215511

--- dev/multicolchars/lib/header-c64.asm
+++
@@ -1,8 +0,0 @@
-;-----------------------------------------------------------
-; Entete de programme assembleur pour demarrage à partir du
-; sur un COMMODORE C64.
-;-----------------------------------------------------------
-*= $801
-.word (+), 10
-.null $9e, "2061"
-+ .word 0
