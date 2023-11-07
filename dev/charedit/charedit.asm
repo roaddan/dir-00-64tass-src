d9f5a8e73b514acfe792ddb23c2877546da13033
Daniel Lafrance <roaddan@gmail.com>
2023-11-06 22:11:26 -0500

    20231106-221126

--- dev/charedit/charedit.asm
+++ dev/charedit/charedit.asm
@@ -9,10 +9,34 @@
 main           .block
                jsr  push
                jsr  screendis
+
+               jsr  copycharset
+
+               ; BASIC -> poke 53272, (peek(53272) and 240) or 12
+               lda  vicmemptr      ;$d018, 53272               
+               ; vicmemptr
+               ;  +-------+-------+-------+-------+-------+-------+-------+-------+
+               ;  |   7   |   6   |   5   |   4   |   3   |   2   |   1   |   0   |
+               ;  +-------+-------+-------+-------+-------+-------+-------+-------+
+               ;  |  txt  |  txt  |  txt  |  txt  | chars | chars | chars |       | 
+               ;  |  scr  |  scr  |  scr  |  scr  |  def  |  def  |  def  |   X   |
+               ;  | bit 3 | bit 2 | bit 1 | bit 0 | bit 2 | bit 1 | bit 0 |       |
+               ;  +-------+-------+-------+-------+-------+-------+-------+-------+
+               and  #%11110000     ; On conserve les bits 7654 de ce registre ...
+                                   ; ... afin de conserver la mémoire vidéo à $0400.
+               ora  #charsdef      ; on place les bits 3210 à %xxxx001x ce qui ...
+                                   ; ... sélectionne la mémoire du bitmap des ...
+                                   ; ... charactères à $0800. 
+               sta  vicmemptr      ; $d018, 53272
+
+
+
+
                jsr  scrmaninit
 
-               jsr  setscreenptr
-;              jsr  copycharset
+
+;               jsr  setscreenptr
+
                jsr  staticscreen
                jsr  screenena
 
@@ -31,7 +55,6 @@ main           .block
 ;-------------------------------------------------------------------------------
 ;
 ;-------------------------------------------------------------------------------
-scrnnewram     =  $0400
 setscreenptr   .block
                jsr  push
 
@@ -44,14 +67,14 @@ setscreenptr   .block
                ora  #$00000011
                sta  cia2ddra  ;$dd02, 56578 cia2 data direction A
                
-               ; BASIC -> poke 56576, (peek(56576)and252) or 0
+               ; BASIC -> poke 56576, (peek(56576) and 252) or 0
                lda  cia2pra   ;$dd00, 56576 cia2 dataport A
                and  #%11111100
                ora  #%00000000
                sta  cia2pra   ;$dd00, 56576 cia2 dataport A
                
-               ; BASIC -> poke 53272, (peek(53272) and 240) or 2
-               lda  vicmemptr ;$d018, 53272               
+               ; BASIC -> poke 53272, (peek(53272) and 240) or 12
+               lda  vicmemptr      ;$d018, 53272               
                ; vicmemptr
                ;  +-------+-------+-------+-------+-------+-------+-------+-------+
                ;  |   7   |   6   |   5   |   4   |   3   |   2   |   1   |   0   |
@@ -60,28 +83,38 @@ setscreenptr   .block
                ;  |  scr  |  scr  |  scr  |  scr  |  def  |  def  |  def  |   X   |
                ;  | bit 3 | bit 2 | bit 1 | bit 0 | bit 2 | bit 1 | bit 0 |       |
                ;  +-------+-------+-------+-------+-------+-------+-------+-------+
-               and  #%11110000
-               ora  #%00000010
-               sta  vicmemptr; $d018, 53272
+               and  #%11110000     ; On conserve les bits 7654 de ce registre ...
+                                   ; ... afin de conserver la mémoire vidéo à $0400.
+               ora  #charsdef      ; on place les bits 3210 à %xxxx001x ce qui ...
+                                   ; ... sélectionne la mémoire du bitmap des ...
+                                   ; ... charactères à $0800. 
+               sta  vicmemptr      ; $d018, 53272
 
                ; BASIC -> poke 648, 196               
-               lda  #%11000100     ;196
-               sta  $0288 ;648 - top page of screen memory
+               lda  #%00000100     ;%11000100 ; 196
+
+               sta  $0288          ; On indique au kernal que la mémoire video ...
+                                   ; ... commence à 4 * 256 = 1024
+                                   ; 648 - top page of screen memory
 
                jsr  pop
                rts
                .bend
+scrnnewram     = $0400 
+charsdef       = 12
 ;-------------------------------------------------------------------------------
 ;
 ;-------------------------------------------------------------------------------
 copycharset    .block
                jsr  push
 
-               ; BASIC -> poke 56334, peek(56334) and 254
+               ; On desactive les interruptions du clavier.
+               ; BASIC -> poke 56334, peek(56334) and 254 
                lda  cia1cra        ;$dc0e, 56334 cia1 control register A
                and  #%11111110     ;254
                sta  cia1cra        ;$dc0e, 56334 cia1 control register A
 
+               ; On active la rom des bitmaps charactères.     
                ; BASIC -> poke 1, peek(1) and 251
                lda  u6510map       ;$01
                and  #%11111011     ;251
@@ -90,11 +123,13 @@ copycharset    .block
                ; ici on copy le character-map
                jsr  memcopy
 
+               ; On réactive les i/o en replaçant la rom des 
                ; BASIC -> poke 1, peek(1) or 4
                lda  u6510map       ;$01
                ora  #%00000100
                sta  u6510map       ;$01
 
+               ; On réactive les interruptions du clavier.
                ; BASIC -> poke 56334, peek(56334) or 1
                lda  cia1cra        ;$dc0e, 56334 cia1 control register A
                ora  #%00000001     ;254
@@ -109,8 +144,8 @@ copycharset    .block
 ;-------------------------------------------------------------------------------
 memcopy        .block
                jsr  push
-               
-onemore        lda  startaddr
+
+               lda  startaddr
                sta  zpage1
                lda  startaddr+1
                sta  zpage1+1
@@ -119,7 +154,8 @@ onemore        lda  startaddr
                lda  destaddr+1
                sta  zpage2+1
                ldy  #$00
-               lda  (zpage1),y
+
+onemore        lda  (zpage1),y
                sta  (zpage2),y
                jsr  inczp1
                jsr  inczp2
@@ -135,9 +171,196 @@ onemore        lda  startaddr
                jsr  pop
                rts
                .bend
-startaddr      .word     53248
-destaddr       .word     53248-2048
-stopaddr       .word     55296
+
+bitmapmem =    charsdef * 1024
+startaddr      .word     $d000     ; 53248
+destaddr       .word     bitmapmem ; 12288     
+stopaddr       .word     $d800     ; 55296 
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+staticscreen   .block
+               #changebord vgris1
+               #changeback vgris
+               #uppercase
+               jsr  showlines
+               jsr  showallchars
+               jsr  showgrid
+               jsr  showfkeys
+               #affichemesg quit_msg
+               #locate   0,7
+               rts
+               .bend
+
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+showfkeys      .block
+               jsr  push
+               lda  fkeyset
+               cmp  #$0
+               bne  secondks
+               #printcxy f1abutton
+               #printcxy f2abutton
+               #printcxy f3abutton
+               #printcxy f4abutton
+               #printcxy f5abutton
+               #printcxy f6abutton
+               #printcxy f7abutton
+               #printcxy f8abutton
+               jmp end
+secondks       #printcxy f1bbutton
+               #printcxy f2bbutton
+               #printcxy f3bbutton
+               #printcxy f4bbutton
+               #printcxy f5bbutton
+               #printcxy f6bbutton
+               #printcxy f7bbutton
+               #printcxy f8bbutton
+end            jsr  pop
+               rts
+               .bend
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+showallchars   .block
+               jsr push
+               #locate   0,0
+               ldx  #$00
+nextc          txa  
+               sta  scrnnewram,x
+               inx
+               cpx  #$80
+               bne  nextc
+               jsr  pop
+               rts
+               .bend
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+showlines      .block
+hline1=4
+hline2=6
+hline3=18
+vlinepos=16
+vzplit=scrnnewram+(6*40)+8
+               jsr  push
+               ldx  #40
+               lda  #64
+nextl          sta  scrnnewram+(40*hline1)-1,x  ;On imprime les deux grande
+               sta  scrnnewram+(40*hline2)-1,x  ; lignes horizontales
+               dex
+hline          cpx  #vlinepos
+               bpl  notyet
+               sta  scrnnewram+(40*hline3),x    ;On imprime la demiligne horz.
+notyet         cpx  #$00
+               bne  nextl
+               ; on imprime le caractere de jonction (t) au dessus de la 
+               ; ligne vert
+               lda  #<scrnnewram+(40*(hline2))+vlinepos
+               sta  zpage1
+               lda  #>scrnnewram+(40*(hline2))+vlinepos
+               sta  zpage1+1
+               ldy  #0
+               lda  #114
+               sta  (zpage1),y
+               jsr  zp1add40
+               ; on imprime ensuite les characteres de la ligne vert
+               ldx  #24-hline2
+               lda  #93
+another93      sta  (zpage1),y
+               jsr  zp1add40
+               dex
+               bne  another93
+               ; on imprime le joint se la ligne vert et la demi 
+               ; ligne horz
+               lda  #<scrnnewram+(40*(hline3))+vlinepos
+               sta  zpage1
+               lda  #>scrnnewram+(40*(hline3))+vlinepos
+               sta  zpage1+1
+               ldy  #0
+               lda  #115
+               sta  (zpage1),y
+               jsr  pop
+               rts
+               .bend
+
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+showgrid      .block
+gligne=8
+gcol=1
+               jsr  push
+               jsr  screendis
+               lda  #<scrnnewram+(40*(gligne))+gcol
+               sta  zpage1
+               lda  #>scrnnewram+(40*(gligne))+gcol
+               sta  zpage1+1
+               ldx  #8
+nextbox        lda  #101
+               ldy  #9
+               sta  (zpage1),y
+               dey
+               lda  #79
+nextcol        sta  (zpage1),y
+               dey  
+               bne  nextcol
+               jsr  zp1add40
+               dex
+               bne  nextbox
+               ldy  #8
+               lda  #119
+nextlin        sta  (zpage1),y
+               dey  
+               bne  nextlin
+               jsr screenena
+               jsr  pop
+               rts
+               .bend
+
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+zp1add40       .block
+               php
+               pha
+               clc
+               lda  zpage1
+               adc  #40
+               bcc  nocarry
+               inc  zpage1+1
+nocarry        sta  zpage1
+               pla
+               plp
+               rts
+               .bend
+
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+delay          .block
+               jsr  push
+               lda  #$0
+               tax
+               tay
+xagain         dex
+yagain         dey
+               cpy  #$00
+               bne  yagain
+               cpx  #$00
+               bne  xagain
+               jsr  pop
+               rts
+               .bend
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+template       .block
+               jsr  push
+               jsr  pop
+               rts
+               .bend
 ;-------------------------------------------------------------------------------
 ;
 ;-------------------------------------------------------------------------------
@@ -333,191 +556,6 @@ swapit         eor  #$ff
                sta  fkeyset
                jsr  showfkeys
                pla
-               rts
-               .bend
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-staticscreen   .block
-               #changebord vgris1
-               #changeback vgris
-               #uppercase
-               jsr  showlines
-               jsr  showallchars
-               jsr  showgrid
-               jsr  showfkeys
-               #affichemesg quit_msg
-               #locate   0,7
-               rts
-               .bend
-
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-showfkeys      .block
-               jsr  push
-               lda  fkeyset
-               cmp  #$0
-               bne  secondks
-               #printcxy f1abutton
-               #printcxy f2abutton
-               #printcxy f3abutton
-               #printcxy f4abutton
-               #printcxy f5abutton
-               #printcxy f6abutton
-               #printcxy f7abutton
-               #printcxy f8abutton
-               jmp end
-secondks       #printcxy f1bbutton
-               #printcxy f2bbutton
-               #printcxy f3bbutton
-               #printcxy f4bbutton
-               #printcxy f5bbutton
-               #printcxy f6bbutton
-               #printcxy f7bbutton
-               #printcxy f8bbutton
-end            jsr  pop
-               rts
-               .bend
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-showallchars   .block
-               jsr push
-               #locate   0,0
-               ldx  #$00
-nextc          txa  
-               sta  scrnnewram,x
-               inx
-               cpx  #$80
-               bne  nextc
-               jsr  pop
-               rts
-               .bend
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-showlines      .block
-hline1=4
-hline2=6
-hline3=18
-vlinepos=16
-vzplit=scrnnewram+(6*40)+8
-               jsr  push
-               ldx  #40
-               lda  #64
-nextl          sta  scrnnewram+(40*hline1)-1,x  ;On imprime les deux grande
-               sta  scrnnewram+(40*hline2)-1,x  ; lignes horizontales
-               dex
-hline          cpx  #vlinepos
-               bpl  notyet
-               sta  scrnnewram+(40*hline3),x    ;On imprime la demiligne horz.
-notyet         cpx  #$00
-               bne  nextl
-               ; on imprime le caractere de jonction (t) au dessus de la 
-               ; ligne vert
-               lda  #<scrnnewram+(40*(hline2))+vlinepos
-               sta  zpage1
-               lda  #>scrnnewram+(40*(hline2))+vlinepos
-               sta  zpage1+1
-               ldy  #0
-               lda  #114
-               sta  (zpage1),y
-               jsr  zp1add40
-               ; on imprime ensuite les characteres de la ligne vert
-               ldx  #24-hline2
-               lda  #93
-another93      sta  (zpage1),y
-               jsr  zp1add40
-               dex
-               bne  another93
-               ; on imprime le joint se la ligne vert et la demi 
-               ; ligne horz
-               lda  #<scrnnewram+(40*(hline3))+vlinepos
-               sta  zpage1
-               lda  #>scrnnewram+(40*(hline3))+vlinepos
-               sta  zpage1+1
-               ldy  #0
-               lda  #115
-               sta  (zpage1),y
-               jsr  pop
-               rts
-               .bend
-
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-showgrid      .block
-gligne=8
-gcol=1
-               jsr  push
-               jsr  screendis
-               lda  #<scrnnewram+(40*(gligne))+gcol
-               sta  zpage1
-               lda  #>scrnnewram+(40*(gligne))+gcol
-               sta  zpage1+1
-               ldx  #8
-nextbox        lda  #101
-               ldy  #9
-               sta  (zpage1),y
-               dey
-               lda  #79
-nextcol        sta  (zpage1),y
-               dey  
-               bne  nextcol
-               jsr  zp1add40
-               dex
-               bne  nextbox
-               ldy  #8
-               lda  #119
-nextlin        sta  (zpage1),y
-               dey  
-               bne  nextlin
-               jsr screenena
-               jsr  pop
-               rts
-               .bend
-
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-zp1add40       .block
-               php
-               pha
-               clc
-               lda  zpage1
-               adc  #40
-               bcc  nocarry
-               inc  zpage1+1
-nocarry        sta  zpage1
-               pla
-               plp
-               rts
-               .bend
-
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-delay          .block
-               jsr  push
-               lda  #$0
-               tax
-               tay
-xagain         dex
-yagain         dey
-               cpy  #$00
-               bne  yagain
-               cpx  #$00
-               bne  xagain
-               jsr  pop
-               rts
-               .bend
-;-------------------------------------------------------------------------------
-;
-;-------------------------------------------------------------------------------
-template       .block
-               jsr  push
-               jsr  pop
                rts
                .bend
 ;-------------------------------------------------------------------------------
