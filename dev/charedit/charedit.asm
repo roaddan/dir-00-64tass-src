158f0b9dd869675832cb1ee4db21623b3a38e4e2
Daniel Lafrance <roaddan@gmail.com>
2023-11-26 10:38:57 -0500

    20231126-103857

--- dev/charedit/charedit.asm
+++ dev/charedit/charedit.asm
@@ -159,7 +159,7 @@ device         .byte     8
 ; Including my self written libraries.
 ;-------------------------------------------------------------------------------
                .include "routines.asm"
-               .include "messages_fr.asm"
+               .include "messages_en.asm"
                .include "map-c64-kernal.asm"
                .include "map-c64-vicii.asm"
                .include "map-c64-basic2.asm"
