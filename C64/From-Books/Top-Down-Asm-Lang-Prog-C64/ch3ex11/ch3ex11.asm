968df269dd94d0cda3e3a3f64a764929cefafad8
Daniel Lafrance <roaddan@gmail.com>
2024-07-10 19:00:32 -0400

    20240710-190032

---
+++ C64/From-Books/Top-Down-Asm-Lang-Prog-C64/ch3ex11/ch3ex11.asm
@@ -0,0 +1,124 @@
+;-------------------------------------------------------------------------------
+                Version = "20240710-181424-a"
+;-------------------------------------------------------------------------------                .include    "header-c64.asm"
+               .include    "header-c64.asm"
+               .include    "macros-64tass.asm"
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------
+               .enc    none
+               jsr  push
+               jsr  main
+               jsr  pop
+               rts                                         
+*=$c000
+main           .block
+               jsr scrmaninit
+               #disable
+               jsr help
+               jmp  mainout
+               jsr anykey
+               jsr ch3ex11
+               #enable
+               #uppercase
+               jsr  cls
+               #graycolor
+;               jmp b_warmstart
+mainout        rts
+               .bend
+                 
+help           .block      
+               #lowercase
+               jsr cls
+               #print line
+               #print headera
+               #print headerb
+               #print shortcuts
+               #print helptext
+               #print line
+               rts                                
+headera                       ;0123456789012345678901234567890123456789
+               .text          " Top-Down assembly language programming"
+               .byte   $0d
+               .text          "     For the Commodore Vic20 and 64"
+               .byte   $0d
+               .text          "           Book by Ken Skier."
+               .byte   $0d
+               .text          "         ISBN 0-07-057864-8 PBK"
+               .byte   $0d,0
+
+headerb        .text          "             ch3ex11 (p26)"
+               .byte   $0d
+               .text          "        (c) 1979 Brad Templeton"
+               .byte   $0d
+               .text          "     programmed by Daniel Lafrance."
+               .byte   $0d
+               .text   format("        Version: %s.",Version)
+               .byte   $0d,0
+
+shortcuts      .byte   $0d
+               .text          " -------- S H O R T - C U T S ---------"
+               .byte   $0d, $0d
+               .text   format(" Main Run.....: SYS%05d ($%04X)",main, main)
+               .byte   $0d
+               .text   format(" This help....: SYS%05d ($%04X)",help, help)
+               .byte   $0d
+               .text   format(" Clear screen.: SYS%05d ($%04X)",cls, cls)
+               .byte   $0d,0
+helptext       .text   format(" Run ch3ex11..: SYS%05d ($%04X)",ch3ex11, ch3ex11)
+               .byte   $0d, $0d
+               .text   format(" Example......: SYS%05d",ch3ex11)
+               .byte   $0d,0
+line           .text          " --------------------------------------"
+               .byte   $0d,0
+               .bend
+
+*=50000 ;$c400+24        ;To place the function at a specific place in memory.
+;-------------------------------------------------------------------------------
+; This function prints 8 'Z' characters at the cursor position using an indirect 
+; pointer.
+;-------------------------------------------------------------------------------
+
+ch3ex11       .block
+               pha
+               lda vicbordcol
+               sta byte
+               lda #$10
+               sta vicbordcol
+;===============================================================================
+; Example 11 on page 27 starts here
+;-------------------------------------------------------------------------------
+               jmp  init
+pointer   .word     printit
+init           ldx  #0
+load           lda  #'Z'
+useptr         jmp  (pointer)
+; anything that would be inserted here will be jumped over by the pointer.
+               .byte 00,00,00,00,00,00,00,00
+
+printit        jsr  $ffd2
+adhust         inx
+test           cpx #9
+branch         bne useptr
+;-------------------------------------------------------------------------------
+; Example stops here
+;===============================================================================
+               jsr anykey
+               lda byte
+               sta vicbordcol
+               pla
+               rts
+               .bend
+byte           .byte 0
+;-------------------------------------------------------------------------------
+; Je mets les libtrairies à la fin pour que le code du projet se place aux debut
+;-------------------------------------------------------------------------------
+*=$c800        
+               .include "map-c64-kernal.asm"
+               .include "map-c64-vicii.asm" 
+               .include "map-c64-basic2.asm"
+               .include "lib-c64-basic2.asm"
+               .include "lib-cbm-pushpop.asm"
+               .include "lib-cbm-mem.asm"
+               .include "lib-cbm-hex.asm"
+               .include "lib-cbm-keyb.asm"
