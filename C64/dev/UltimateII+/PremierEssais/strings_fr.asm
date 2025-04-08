e8b80ad1d7cf2c957a042146ae3c7bbe00f68c52
Daniel Lafrance <roaddan@gmail.com>
2025-04-08 12:35:27 -0400

    20250408-123527

--- C64/dev/UltimateII+/PremierEssais/strings_fr.asm
+++ C64/dev/UltimateII+/PremierEssais/strings_fr.asm
@@ -50,25 +50,27 @@ uiix           =    1
 lbluiititle    .byte     1,uiix+9,uiiy,18
                .text     " 1541 Ultimate II + "
                .byte     146,0
-lbluiiidenreg  .byte     1,uiix ,uiiy+2
+lbluiiidenreg  .byte     1,uiix ,uiiy+3
                .null     format("Identification..($%04X) -> ", uiiidenreg)
-lbluiistatreg  .byte     1,uiix ,uiiy+4
+lbluiistatreg  .byte     1,uiix ,uiiy+5
                .null     format("Status Commande.($%04X) -> ", uiicmdstat) 
-lbluiistadata  .byte     1,uiix ,uiiy+6
+lbluiistadata  .byte     1,uiix ,uiiy+7
                .null     format("Data reponse....($%04X) -> ", uiirxdata) 
-lbluiirspdata  .byte     1,uiix ,uiiy+8.
+lbluiirspdata  .byte     1,uiix ,uiiy+9.
                .null     format("Status Data.....($%04X) -> ", uiidatastat) 
 ;===============================================
 ; Value text    
 ;===============================================
-txtuiiidenreg  .byte     3,uiix+28,uiiy+2,0
-defuiistatreg  .byte     3,uiix+29,uiiy+3
+txtuiiidenreg  .byte     3,uiix+28,uiiy+3,0
+txtuiistatreg  .byte     3,uiix+27,uiiy+5,0  
+txtuiistadata  .byte     3,uiix+27,uiiy+7,0
+txtuiirspdata  .byte     3,uiix+27,uiiy+9,0
+
+defuiistatreg  .byte     7,uiix+28,uiiy+4
                .null     "AASSEPCB"
-txtuiistatreg  .byte     3,uiix+28,uiiy+4,0  
-txtuiirspdata  .byte     3,uiix+28,uiiy+8,0
-defuiistadata  .byte     3,uiix+29,uiiy+5
+defuiistadata  .byte     7,uiix+28,uiiy+6
                .null     "AASSEPCB"
-txtuiistadata  .byte     3,uiix+28,uiiy+6,0
+
 txtrespponse   .byte     3,uiix+9,uiiy+1,0
 
 
