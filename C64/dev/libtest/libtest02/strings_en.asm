6c3859b13873975d2a2b9640986551387d0ed944
Daniel Lafrance <roaddan@gmail.com>
2025-04-22 13:45:33 -0400

    20250422-134532

---
+++ C64/dev/libtest/libtest02/strings_en.asm
@@ -0,0 +1,83 @@
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------                
+headera                       ;0123456789012345678901234567890123456789
+               .text          "     1541 Ultimate II + First Try "
+               .byte     $0d  
+               .text          " Cartridge and API by Gideon Zweijtzer."
+               .byte     $0d
+               .text          "    API Version 1.0, Feb. 1st 2013"
+               .byte     $0d,0
+
+headerb        .text          "             essai01.prg "
+               .byte     $0d
+               .text          "       (c) 2025 Daniel Lafrance"
+               .byte     $0d
+               .text   format("       Version: %s",Version)
+               .byte     $0d,0
+
+shortcuts      .byte     $0d
+               .byte     ucurkey,ucurkey
+               .byte     rcurkey,rcurkey,rcurkey,rcurkey
+               .byte     rcurkey,rcurkey,rcurkey,rcurkey,rcurkey      
+               .text          " R A C C O U R C I S "
+               .text          "  S H O R T C U T S  "
+               .byte     $0d
+               .text   format(" essai01..: SYS%05d ($%04X)",main, main)
+               .byte     $0d
+               .text   format(" help.....: SYS%05d ($%04X)",aide, aide)
+               .byte     $0d
+               .text   format(" cls......: SYS%05d ($%04X)",cls, cls)
+               .byte     $0d,0
+aidetext       .text   format(" Execute: SYS%05d ($%04X)",essai01, essai01)
+               .byte     $0d, $0d
+               .text   format("    ex.: SYS%05d",essai01)
+               .byte     $0d
+               .text   format("    for i=0to100:SYS%05d:next",essai01)
+               .byte     $0d,0
+line           .byte     $20,192,192,192,192,192,192,192,192,192
+               .byte     192,192,192,192,192,192,192,192,192,192
+               .byte     192,192,192,192,192,192,192,192,192,192
+               .byte     192,192,192,192,192,192,192,192,192
+               .byte     $0d,0
+uiiconnected   .null     "Installed"
+uiiunconnected .null     "Uninstalled"                  
+
+uiiy           =    1
+uiix           =    1  
+;===============================================
+; Static text    
+;===============================================
+lbluiititle    .byte     1,uiix+9,uiiy,18
+               .text     " 1541 Ultimate II + "
+               .byte     146,0
+lbluiiidenreg  .byte     1,uiix ,uiiy+3
+               .null     format("Identification..($%04X) -> ", uiiidenreg)
+lbluiistatreg  .byte     1,uiix ,uiiy+5
+               .null     format("Commande Status.($%04X) -> ", uiicmdstat) 
+lbluiistadata  .byte     1,uiix ,uiiy+7
+               .null     format("Data Response...($%04X) -> ", uiirxdata) 
+lbluiirspdata  .byte     1,uiix ,uiiy+9.
+               .null     format("Data Status.....($%04X) -> ", uiidatastat) 
+;===============================================
+; Value text    
+;===============================================
+txtuiiidenreg  .byte     3,uiix+28,uiiy+3,0
+txtuiistatreg  .byte     3,uiix+27,uiiy+5,0  
+txtuiistadata  .byte     3,uiix+27,uiiy+7,0
+txtuiirspdata  .byte     3,uiix+27,uiiy+9,0
+
+defuiistatreg  .byte     3,uiix+28,uiiy+4
+               .null     "AASSEPCB"
+defuiistadata  .byte     3,uiix+28,uiiy+6
+               .null     "AASSEPCB"
+
+txtrespponse   .byte     3,uiix+9,uiiy+1,0
+
+
+;uiictrlreg     =    $df1c     ;(Write)
+;uiistatreg     =    $df1c     ;(Read)   default $00
+;uiicmddata     =    $df1d     ;(Write)
+;uiiidenreg     =    $df1d     ;(Read)   default $c9
+;uiirspdata     =    $df1e     ;(Read only)
+;uiistadata     =    $df1f     ;(Read only)
---
+++ C64/dev/libtest/libtest02/strings_fr.asm
@@ -0,0 +1,40 @@
+;-------------------------------------------------------------------------------
+;
+;-------------------------------------------------------------------------------                
+headera                       ;0123456789012345678901234567890123456789
+               .byte     b_home,b_crsr_right,b_crsr_right,b_crsr_right,b_crsr_right
+               .text              " Commodore 64 test de libraries "
+               .byte     $0d,0
+
+headerb        .text          "               libtest03 "
+               .byte     $0d
+               .text          "       (c) 2025 Daniel Lafrance"
+               .byte     $0d
+               .text   format("       Version: %s",Version)
+               .byte     $0d,0
+
+shortcuts      .byte     $0d
+               .byte     ucurkey,ucurkey
+               .byte     rcurkey,rcurkey,rcurkey,rcurkey
+               .byte     rcurkey,rcurkey,rcurkey,rcurkey,rcurkey      
+               .text          " R A C C O U R C I S "
+               .byte     $0d
+               .text   format(" libtest03: SYS%05d (jsr $%04X)",main, main)
+               .byte     $0d
+               .text   format(" aide.....: SYS%05d (jsr $%04X)",aide, aide)
+               .byte     $0d
+               .text   format(" cls......: SYS%05d (jsr $%04X)",cls, cls)
+               .byte     $0d,0
+aidetext       .text   format(" Lancement: SYS%05d (jsr $%04X)",libtest03, libtest03)
+               .byte     $0d, $0d
+                .text   format("    ex.: SYS%05d",libtest03)
+                .byte     $0d
+                .text   format("    for i=0to100:SYS%05d:next",libtest03)
+                .byte     $0d,0
+line            .byte     $20,192,192,192,192,192,192,192,192,192
+                .byte     192,192,192,192,192,192,192,192,192,192
+                .byte     192,192,192,192,192,192,192,192,192,192
+                .byte     192,192,192,192,192,192,192,192,192
+                .byte     $0d,0
+
+dataloc         .byte       1,0,0,0
