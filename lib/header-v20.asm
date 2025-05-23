167631d6fe6f5f64c4e8271adfb93b6fdeb7c0ad
Daniel Lafrance <roaddan@gmail.com>
2025-05-23 18:16:57 -0400

    Mis à jour par D.L.: 20250523-181657.

--- lib/header-v20.asm
+++ lib/header-v20.asm
@@ -4,9 +4,9 @@
 ; Cernière m.à j. : 20250521-222823
 ; Inspiration ....: 
 ;--------------------------------------------------------------------------------
-; Entete de programme assembleur pour demarrage à partir du
-; BASIC 2.0 du Commodore Vic20 sans expantion de RAM.
-;-----------------------------------------------------------
+; Entête de programme assembleur pour demarrage à partir du BASIC 2.0 du 
+; Commodore Vic20 sans expention de RAM.
+;--------------------------------------------------------------------------------
 *= $1001
 .word (+), 10       ;$1001 : Debut + 10 =$100b, $0010
 .null $9e, "4109"   ;$1005 : "sys4109" = jsr $100d
