39e2060a4c8a19c80037044f2cc24a093b62db00
Lafrance Daniel <roaddan@gmail.com>
2025-09-26 16:50:04 -0400

    20250926-165004

--- lib/lib-c64-drawbox.asm
+++ lib/lib-c64-drawbox.asm
@@ -26,161 +26,210 @@
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
+; Macro: drawbox
+; Description: Macro qui initialise les paramètres de la fenetre à être 
+;              dessinée et appelle la fonction qui dessine la fenêtre.
+; Paramêtres d'entrée: 
+;              1,2) top et left....: Le coin suppérieur droit de la fenëtre.
+;              3,4) width et height: La largeur et la hauteur de la fenêtre.
+;              5) colour...........: La couleur de la fenêtre. >16=inverse.
+;              6) titre............: Titre de la fenêtre.
+;-------------------------------------------------------------------------------
+drawbox        .macro top, left, width, height, colour, title
+               php                 ; Sauvegarde les registres.
+               pha
+               lda #\top           ; Initialise le paramètre top et la ...
+               sta drawbox_top
+               sta drawbox_curline ; ... position de la ligne à traiter.
+               lda #\left          ; Initialise le paramètre left.
+               sta drawbox_left
+               lda #\width         ; Initialise le paramètre width.
+               sta drawbox_width
+               lda #\height        ; Initialise le paramètre height.
+               sta drawbox_height
+               lda #\colour        ; Initialise le paramètre colour.
+               sta drawbox_colour
+               pla                 ; Récupère les registres.
+               plp
+               jsr db_drawbox      ; Dessine la fenêtre.
+                                   ; Affiche le titre.
+               #print_cxy \colour,\left+1,\top,\title
+               .endm
+
+;-------------------------------------------------------------------------------
 ; Fonction: db_tline
 ; Description:  Fonction qui dessine la ligne suppérieur d'une boite texte en 
 ;               utilisant les caractères graphiques disponnible dand les modes
 ;               majuscule et minuscule.
-; Paramêtres d'entrée: 
-; 
 ;-------------------------------------------------------------------------------
-drawbox  .macro top, left, width, height, colour, titre
-            php
-            pha
-            lda #\top
-            sta drawbox_top
-            sta drawbox_curline
-            lda #\left
-            sta drawbox_left
-            lda #\width
-            sta drawbox_width
-            lda #\height
-            sta drawbox_height
-            lda #\colour
-            sta drawbox_colour
-            pla
-            plp
-            jsr db_drawbox
-            #print_cxy \colour,\left+1,\top,\titre
+db_tline       .block
+               php            ; Sauvegarde les registres.
+               pha
+               lda #tleft     ; Coin suppérieur gauche.
+               sta db_left
+               lda #hline     ; Ligne horizontale.
+               sta db_mid
+               lda #tright    ; Coin suppérieur droit.
+               sta db_right
+               plp            ; Récupère les registres.
+               pla
+               jsr db_drawline
+               rts
+               .bend          
 
-            .endm
-db_tline    .block
-            php
-            pha
-            lda #tleft
-            sta db_left
-            lda #hline
-            sta db_mid
-            lda #tright
-            sta db_right
-            plp
-            pla
-            jsr db_drawline
-            rts
-            .bend          
+;-------------------------------------------------------------------------------
+; Fonction: db_bline
+; Description:  Fonction qui dessine la ligne inférieure d'une boite texte en 
+;               utilisant les caractères graphiques disponnible dand les modes
+;               majuscule et minuscule.
+;-------------------------------------------------------------------------------
+db_bline       .block
+               php            ; Sauvegarde les registres.
+               pha
+               lda #bleft     ; Coin inférieur gauche.
+               sta db_left
+               lda #hline     ; Ligne horizontale.
+               sta db_mid
+               lda #bright    ; Coin inférieur droit.
+               sta db_right
+               plp            ; Récupère les registres.
+               pla
+               jsr db_drawline
+               rts
+               .bend
 
-db_bline    .block
-            php
-            pha
-            lda #bleft
-            sta db_left
-            lda #hline
-            sta db_mid
-            lda #bright
-            sta db_right
-            plp
-            pla
-            jsr db_drawline
-            rts
-            .bend
+;-------------------------------------------------------------------------------
+; Fonction: db_eline
+; Description:  Fonction qui dessine une ligne vide d'une boite texte en 
+;               utilisant les caractères graphiques disponnible dand les modes
+;               majuscule et minuscule.
+;-------------------------------------------------------------------------------
+db_eline       .block
+               php            ; Sauvegarde les registres.
+               pha
+               lda #vline     ; Ligne verticale.
+               sta db_left
+               lda #space     ; Caractère espace.
+               sta db_mid
+               lda #vline     ; Ligne verticale.
+               sta db_right
+               plp            ; Récupère les registres.
+               pla
+               jsr db_drawline
+               rts
+               .bend
 
-db_eline    .block
-            php
-            pha
-            lda #vline
-            sta db_left
-            lda #$20
-            sta db_mid
-            lda #vline
-            sta db_right
-            plp
-            pla
-            jsr db_drawline
-            rts
-            .bend
+;-------------------------------------------------------------------------------
+; Fonction: db_mline
+; Description:  Fonction qui dessine une ligne horozontale d'une boite texte en 
+;               utilisant les caractères graphiques disponnible dand les modes
+;               majuscule et minuscule.
+;-------------------------------------------------------------------------------
+db_hline       .block
+               php            ; Sauvegarde les registres.
+               pha
+               lda #hleft     ; Extrémité gauche d'une ligne médianne.
+               sta db_left
+               lda #hline     ; Ligne horizontale.
+               sta db_mid
+               lda #hright    ; Extrémité droite d'une ligne médianne.
+               sta db_right
+               plp            ; Récupère les registres.
+               pla
+               jsr db_drawline
+               rts
+               .bend
 
-db_mline    .block
-            php
-            pha
-            lda #mleft
-            sta db_left
-            lda #hline
-            sta db_mid
-            lda #mright
-            sta db_right
-            plp
-            pla
-            jsr db_drawline
-            rts
-            .bend
+;-------------------------------------------------------------------------------
+; Fonction: db_drawline
+; Description:  Fonction qui dessine une ligne horozontale d'une boite texte en 
+;               utilisant les caractères graphiques disponnible dand les modes
+;               majuscule et minuscule.
+;
+; Note :  Appelé par les fonction suivantes:
+;         db_tline, db_bline, db_eline ou db_hline
+;-------------------------------------------------------------------------------
+db_drawline    .block
+               jsr pushreg         ; Sauvegarde les registres.
+               lda bascol          ; Sauvegarde de la ...
+               sta drawbox_bascol  ; ... couleur basic.
+               lda drawbox_colour  ; Sélectionne la couleur...     
+               sta bascol          ; ... de la fenêtre.
+               cmp #$10            ; Couleur inverse vidéo?
+               bmi noreverse       ; Non ...
+               lda #18             ; Oui on place basic en inverse ...
+               jsr chrout          ; Vidéo.
+noreverse      ldx drawbox_curline ; On positionne le curseur au ...
+               ldy drawbox_left    ; ... coin suppérieur gauche ...
+               clc                 ; ... de la fenêtre.
+               jsr plot
+               lda db_left         ; On dessine le premier caractère.
+               jsr chrout
+               lda db_mid          ; On dessine la ligne horizontale ...
+               ldx drawbox_width   ; ... selon la largeur voulue ...
+               dex                 ; ... de la fenêtre moins les ...
+               dex                 ; ... deux caractères d'extrémité.
+               jsr putnch
+               lda db_right        ; On dessine le dernier caractère.
+               jsr chrout
+               lda #146            ; On met fin à l'inverse vidéo.
+               jsr chrout
+               lda drawbox_bascol  ; On récupère et replace la ...
+               sta bascol          ; ... couleur de basic.
+               inc drawbox_curline ; Passe à la ligne suivante.
+               jsr popreg          ; Récupère les registres.
+               rts
+               .bend
 
+;-------------------------------------------------------------------------------
+; Fonction: db_drawbox
+; Description:  Fonction qui dessine une ligne horozontale d'une boite texte en 
+;               utilisant les caractères graphiques disponnible dand les modes
+;               majuscule et minuscule.
+;
+; Note :  Appelé par la macro drawbox.
+;         
+; Utilise les fonctions : db_tline, db_bline, db_eline ou db_hline
+;-------------------------------------------------------------------------------
+db_drawbox     .block
+               jsr pushreg         ; Sauvegarde les registres.
+               ldx drawbox_height  ; Récupère la hauteur de la fenètre.
+               jsr db_tline        ; Dessine la ligne suppérieure.
+               dex                 ; Soustrait 2 pour tenir compte des ...
+               dex                 ; lignes suppérieures et inférieures.
+               beq lastline        ; Si 0 pas de ligne vide dans la fenètre.
+moreline       jsr db_eline        ; Dessine une ligne vide de la boîte.
+               dex                 ; Une ligne de moins à faire.
+               bne moreline        ; On passe à la prochaine s'il en reste.
+lastline       jsr db_bline        ; Dessine la ligne inférieure.
+               jsr popreg          ; Récupère les registres.
+               rts
+               .bend
 
-db_drawline .block
-            jsr pushreg
-            ; On sélectionne la couleur
-            lda bascol
-            sta drawbox_bascol
-            lda drawbox_colour           
-            sta bascol
-            cmp #$10
-            bmi noreverse
-            lda #18
-            jsr chrout
-noreverse   ; On positionne le curseur à gauche de la boite
-            ldx drawbox_curline
-            ldy drawbox_left
-            clc 
-            jsr plot
-            ; On dessine le premier caractère.
-            lda db_left
-            jsr chrout
-            ; On dessine la ligne horizontale. 
-            lda db_mid
-            ldx drawbox_width
-            dex
-            dex
-            jsr putnch
-            lda db_right
-            jsr chrout
-            lda #146
-            jsr chrout
-            lda drawbox_bascol
-            sta bascol
-            jsr popreg
-            rts
-            .bend
-db_drawbox  .block
-            jsr pushreg
-            ldx drawbox_height
-            dex
-            jsr db_tline
-            dex
-            beq lastline
-moreline    inc drawbox_curline
-            jsr db_eline
-            dex
-            bne moreline
-lastline    inc drawbox_curline
-            jsr db_bline
+;-------------------------------------------------------------------------------
+; Constantes des caractères de dessin de boîte disponnible dans tous les modes
+; de caractères (majuscule et minuscule).
+;-------------------------------------------------------------------------------
+tleft       =   176      ; Coin suppérieur gauche.
+tright      =   174      ; Coin suppérieur droit.
+bleft       =   173      ; Coin inférieur gauche.
+bright      =   189      ; Coin inférieur droit.
+hleft       =   171      ; Extrémité gauche d'une ligne médianne.
+hright      =   179      ; Extrémité droite d'une ligne médianne.
+vline       =   221      ; Ligne verticale.
+hline       =   192      ; Ligne horizontale.
+space       =    32      ; Caractère espace.
 
-            jsr popreg
-            rts
-            .bend
-
-tleft       =   176
-tright      =   174
-bleft       =   173
-bright      =   189
-mleft       =   171
-mright      =   179
-vline       =   221
-hline       =   192
-db_left         .byte   0
-db_right        .byte   0
-db_mid          .byte   0
-drawbox_top     .byte   0
-drawbox_left    .byte   0
-drawbox_width   .byte   0
-drawbox_height  .byte   0
-drawbox_curline .byte   0
-drawbox_colour  .byte   0
+;-------------------------------------------------------------------------------
+; Variables globales des paramètres de dessin de fenetre texte.
+;-------------------------------------------------------------------------------
+db_left         .byte   0     ; Caractère Gauche de la ligne courante. 
+db_right        .byte   0     ; Caractère droit de la ligne courante.
+db_mid          .byte   0     ; Caractère central de la ligne courante.
+drawbox_top     .byte   0     ; Coordonné du haut de la boite.
+drawbox_left    .byte   0     ; Coordonné de la gauche de la boîte.
+drawbox_width   .byte   0     ; Largeur de la boîte.
+drawbox_height  .byte   0     ; Hauteur de la boîte.
+drawbox_colour  .byte   0     ; Couleur de la boîte.
+drawbox_curline .byte   0     ; No de la ligne courante à dessiner.
 drawbox_bascol  .byte   0
