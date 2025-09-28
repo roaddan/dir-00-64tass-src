7683a76195ec722ed2c2d22ce67936e593238747
Lafrance Daniel <roaddan@gmail.com>
2025-09-28 16:47:14 -0400

    20250928-164714

--- lib/map-c64-kernal.asm
+++ lib/map-c64-kernal.asm
@@ -1,5 +1,5 @@
 ;-------------------------------------------------------------------------------
-; Scripteur ......: Daniel Lafrance, G9B-0S5, canada.
+; Scripteur ......: Daniel Lafrance, Québec, canada.
 ; Nom du fichier .:
 ; Cernière m.à j. : 
 ; Inspiration ....: 
