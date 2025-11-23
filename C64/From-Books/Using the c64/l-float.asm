6f893e96b535ee655342274c7c50b9b40f113846
Lafrance Daniel <roaddan@gmail.com>
2025-11-23 15:52:51 -0500

    20251123-155251

--- C64/From-Books/Using the c64/l-float.asm
+++ C64/From-Books/Using the c64/l-float.asm
@@ -4,10 +4,58 @@
 ; Version..: 1.0.1 pour T.M.P.
 ; Revision.: 20251117
 ;---------------------------------------
-prnf1dbl
-            .block
-            jsr pushregs
-
-            jsr popreg
-            rts
-            .bend
+clrbasbuf   
+;---------------------------------------
+        .block
+        php
+        pha
+        tya
+        pha
+        lda #$00
+        ldy #$59
+again   sta $0200,y
+        dey
+        bne again
+        pla
+        tay
+        pla
+        plp
+        rts
+        .bend
+;---------------------------------------
+getfac1
+;---------------------------------------
+        .block
+        jsr pushregs 
+        jsr clrbasbuf
+        jsr prompt
+        stx $7a
+        sty $7b
+        jsr chrget
+        jsr fasctf1
+        jsr popregs
+        .bend
+;---------------------------------------
+prnfac1
+;---------------------------------------
+        .block
+        jsr pushregs
+        jsr ff1tasc
+        ldy #$01
+        ldx #$00
+        lda #$03
+        sta kcol
+        jsr putsyx
+        lda #$01
+        sta kcol
+        jsr popregs
+        rts
+        .bend
+;---------------------------------------
+floattest
+        .block
+        jsr clrbasbuf
+        jsr getfac1
+        jsr prnfac1
+        rts
+        .bend
