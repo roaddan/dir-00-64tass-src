87b51ed92fb86194bc17bc9c961bf76bc14e6218
Lafrance Daniel <roaddan@gmail.com>
2025-11-23 13:57:05 -0500

    20251123-135705

---
+++ C64/From-Books/Using the c64/l-float.asm
@@ -0,0 +1,6 @@
+;---------------------------------------
+; fichier..: l-float.asm (seq)
+; Auteur...: Daniel Lafrance
+; Version..: 1.0.1 pour T.M.P.
+; Revision.: 20251117
+;---------------------------------------
