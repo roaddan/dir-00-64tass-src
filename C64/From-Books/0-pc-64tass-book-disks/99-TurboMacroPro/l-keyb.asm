705d74ac6f9e28240981be768c1a17af07f3fdc9
Lafrance Daniel <roaddan@gmail.com>
2025-12-02 23:33:34 -0500

    20251202-233334

--- C64/From-Books/0-pc-64tass-book-disks/99-TurboMacroPro/l-keyb.asm
+++ C64/From-Books/0-pc-64tass-book-disks/99-TurboMacroPro/l-keyb.asm
@@ -6,19 +6,31 @@
 ; inspiration ....: 
 ;---------------------------------------
 ;---------------------------------------
-waitstop
+; Vide le tampon du clavier
+clrkbbuf
 ;---------------------------------------
             .block
-            php   ;\ sauve les flags
-            pha   ;/  et l'acc.
-            jsr clrkbbuf
-wait        jsr k_stop;cherche la clef
-                      ; [run/stop].
-            bne  wait ;loop pas pesee.
-            pla    ;\ recup. acc et 
-            plp    ;/  les flags.
-            rts
+            php
+            pha
+            lda #0
+            sta 198
+            pla
+            plp
+            rta
             .bend
+;---------------------------------------
+waitstop
+;---------------------------------------
+          .block
+          php    ;\ sauve les flags
+          pha    ;/  et l'acc.
+          jsr clrkbbuf
+wait      jsr k_stop  ; Veri [run/stop]
+          bne  wait   ;loop pas pesee.
+          pla    ;\ recup. acc et 
+          plp    ;/  les flags.
+          rts
+          .bend
 ;---------------------------------------
 ; attend qu'une touche du clavier soit 
 ; appuyee/relachee.
@@ -35,7 +47,7 @@ wait        lda 203 ;lit la matrice de
                     ; clef appuyee.
             beq wait; on attend qu'il y 
                     ; en aie une.
-            jsr kbfree ; on attend que 
+            ;jsr kbfree ; on attend que 
                        ;le clavier soit 
                        ;relache.
             jsr clrkbbuf
@@ -72,19 +84,7 @@ try         jsr getin ;tente de lire
                       ; une clef.
             cmp #0  ; 0 si aucune.
             beq try ; aucune, on attend.
-            php     ; recup. les flags.
-            rts
-            .bend
-;---------------------------------------
-clrkbbuf
-;---------------------------------------
-            .block
-            php     ;\ sauve les flags 
-            pha     ;/  et l'acc.
-            lda #0  ;\ efface le tampon 
-            sta 198 ;/  du clavier.
-            pla     ;\ recup. l'acc et 
-            plp     ;/  les flags.
+            plp     ; recup. les flags.
             rts
             .bend
 ;---------------------------------------
@@ -101,48 +101,22 @@ wait        jsr getin; tente lire clef.
             ;jsr chrout  ; l'affiche.
             cmp clef ;compare avec clef
             bne wait ;pas la bonne. 
-            jsr chrout ; l'affiche.
+            ;jsr chrout ; l'affiche.
             pla      ;\ recup. l'acc et 
             plp      ;/  les flags.
             rts
             .bend
 ;---------------------------------------
-; attend la touche espace appuyee.
+; attend que la touche  dans acc soit 
+; appuyee.
 waitspace
 ;---------------------------------------
             .block
-            php     ;\ sauve les flags 
-            pha     ;/  et l'acc.
-            lda #0  ;\ efface le tampon 
-            sta 198 ;/  du clavier.
-nospace     lda #203; lit la matrice. 
-            cmp #60 ;60 = espace pesee 
-            bne nospace    
-            pla     ;\ recup. l'acc et 
-            plp     ;/  les flags.
-            .bend
-
-;---------------------------------------
-waitreturn
-;---------------------------------------
-            .block
-            php     ;\ sauve les flags 
-            pha     ;/  et l'acc.
-            lda #0  ;\ efface le tampon 
-            sta 198 ;/  du clavier.
-wait        lda #203; lit la matrice
-            cmp #1  ; 1  = [return]. 
-            php     ; sauve les flags 
-                    ; pour comparaison
-            lda #0  ;\ efface le tampon 
-            sta 198 ;/  du clavier.
-            plp     ;recup. les flags 
-                    ; pour comparaison
-            bne wait   
-            pla     ;\ recup. l'acc et 
-            plp     ;/  les flags.
+            lda #$20
+            jsr waitkey
+            jsr clrkbbuf
+            rts
             .bend
 ;---------------------------------------
 clef        .byte   0
-thecount    .byte   $01
 ;---------------------------------------
