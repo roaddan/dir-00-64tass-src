a4f760c7ed99d2d50a26eb5b3e32791b28d9ec1f
Lafrance Daniel <roaddan@gmail.com>
2025-12-16 00:37:51 -0500

    20251216-003751

--- C64/From-Books/0-pc-64tass-book-disks/99-TurboMacroPro/progvars.asm
+++ C64/From-Books/0-pc-64tass-book-disks/99-TurboMacroPro/progvars.asm
@@ -14,18 +14,81 @@ counter  .byte $00
 baseaddr .word $2000 ;8192
 scrwidth .word $0140 ;320
 ;---------------------------------------
-dmphead .byte $20,18,5
-        .text "ADRES"
+dmphead .byte 18,5
+        .text "adres"
         .byte 159
         .text " 00 01 02 03"
         .text " 04 05 06 07 "
         .byte 153
         .text "petsciis"
-        .byte 146,$0d,0
-menu    .byte $0d
-        .text ""
-        .byte $0d
-        .byte $0d
-        .byte $0d
-        .byte $0d
-        .byte $00
+        .byte 146,0
+dmphead2 .byte 18,5
+        .text "     "
+        .byte 159
+        .text " 08 09 0A 0B"
+        .text " 0C 0D 0E 0F "
+        .byte 153
+        .text "        "
+        .byte 146,0
+menu0   .byte 1,18
+        .byte kjaune,176
+        .byte 192,192,192
+        .byte 192,192,192,192,192,192
+        .byte 192,192,192,192,192,192
+        .byte 18
+        .text " MENU "
+        .byte 146
+        .byte 192,192,192,192,192,192
+        .byte 192,192,192,192,192,192
+        .byte 192,192,192
+        .byte 174
+        .byte 0
+menu1   .byte 1,19
+        .byte kjaune,221,kblanc
+        .text " [F1] +128 octets"
+        .byte kjaune,182,kblanc
+        .text " [F2] -128 octets "
+        .byte kjaune,221,kblanc
+        .byte 0        
+menu2   .byte 1,20
+        .byte kjaune,221,kblanc
+        .text " [F3] +256 octets"
+        .byte kjaune,182,kblanc
+        .text " [F4] -256 octets "
+        .byte kjaune,221,kblanc
+        .byte 0        
+menu3   .byte 1,21
+        .byte kjaune,221,kblanc
+        .text " [F5] +512 octets"
+        .byte kjaune,182,kblanc
+        .text " [F6] -512 octets "
+        .byte kjaune,221,kblanc
+        .byte 0        
+menu4   .byte 1,22
+        .byte kjaune,221,kblanc
+        .text " [F7]+4096 octets"
+        .byte kjaune,182,kblanc
+        .text " [F8]-4096 octets "
+        .byte kjaune,221,kblanc
+        .byte 0        
+menu5   .byte 1,23
+        .byte kjaune,221,kblanc
+        .text " [N] entrer adre."
+        .byte kjaune,182,kblanc
+        .text " [Q] Quitter      "
+        .byte kjaune,221,kblanc
+        .byte 0        
+menu6   .byte 1,24
+        .byte kjaune,173
+        .byte 192,192,192,192,192,192
+        .byte 192,192,192,192,192,192
+        .byte 192
+        .byte 18
+        .text " DLBUG-64 "
+        .byte 146,192
+        .byte 192,192,192,192,192,192
+        .byte 192,192,192,192,192,192
+        .byte 189,kblanc
+        .byte 0
+
+;---------------------------------------
---
+++ lib/l-drawbox.asm
@@ -0,0 +1,247 @@
+;---------------------------------------
+;Nom du fichier ..: l-drawbox.asm
+;Scripteur .......: Daniel Lafrance
+;Version .........: 0.1.0
+;Dernière m.à j. .: 20250926-165220
+;Inspiration  ....: 
+;---------------------------------------
+;lib-c64-drawbox.asm - 
+;Fonctions d'affichage de boites par 
+;l'utilisation des routines du kernal 
+;du Commodores 64 et 64c. 
+;
+;Note: Compatibilité avec le Vic-20 à 
+;vérifier.
+;---------------------------------------
+;Pour l'utilisation de ce fichier dans 
+;Turbo-macro-pro ou avec 64tass 
+;utilisez la syntaxes ... 
+;Dépendances:
+;---------------------------------------
+;---------------------------------------
+;Macro: drawbox
+;Description: 
+;Macro qui initialise les paramètres de
+;la boite à être dessinée et appelle 
+;la fonction qui dessine la fenêtre.
+;Paramêtres d'entrée: 
+; 1,2) top et left: 
+;  Le coin supp. droit de la boite
+; 3,4) width et height: La largeur et 
+;      la hauteur de la fenêtre.
+; 5) colour: La couleur de la fenêtre. 
+;      >16=inverse.
+; 6) titre: Titre de la fenêtre.
+;---------------------------------------
+drawbox .macro tp,lf,wd,ht,co,ti
+        php         ;Sauve registres.
+        pha
+        lda #\tp    ;Init lparam top
+        sta dbtop
+        sta dbclin;position de ligne
+        lda #\lf    ;Initialise left.
+        sta dbleft
+        lda #\wd    ;Initialise width.
+        sta dbwdth
+        lda #\ht    ;Initialise height.
+        sta dbhght
+        lda #\co    ;Initialise couleur.
+        sta dbcoul
+        pla         ;Récupère registres.
+        plp
+        jsr dbdraw  ;Dessine la fenêtre.
+                    ;Affiche le titre.
+        .if \co < 16 
+        lda #18     ;le titre zzzzzz
+        jsr chrout
+        #outcxy \co,\lf+1,\tp,\ti
+        lda #146
+        jsr chrout
+        .else            
+        #outcxy \co,\lf+1,\tp,\ti
+        .endif
+        .endm
+;---------------------------------------
+;Fonction: dbtline
+;Description:  
+;Fonction qui dessine la ligne 
+;supp. d'une boite texte en 
+;utilisant les caractères graphiques 
+;disponnibles dans les modes
+;majuscule et minuscule.
+;---------------------------------------
+dbtline
+        .block
+        php         ;Sauve registres.
+        pha
+        lda #tleft  ;Coin supp gauche.
+        sta cleft
+        lda #hline  ;Ligne horizontale.
+        sta cmid
+        lda #tright ;Coin supp droit.
+        sta cright
+        plp         ;Récup registres.
+        pla
+        jsr dbdrawline
+        rts
+        .bend          
+;---------------------------------------
+;Fonction: dbbline
+;Description:  
+;Fonction qui dessine la ligne 
+;inf.e d'une boite texte en 
+;utilisant les caractères graphiques 
+;disponnibles dans les modes
+;majuscule et minuscule.
+;---------------------------------------
+dbbline 
+        .block
+        php         ;Sauve registres
+        pha
+        lda #bleft  ;Coin inf. gauche.
+        sta cleft
+        lda #hline  ;Ligne horizontale.
+        sta cmid
+        lda #bright ;Coin inf. droit.
+        sta cright
+        plp         ;Récup registres
+        pla
+        jsr dbdrawline
+        rts
+        .bend
+;---------------------------------------
+;Fonction: dbeline
+;Description:  
+;Fonction qui dessine une ligne vide 
+;d'une boite texte en utilisant les 
+;caractères graphiques disponnibles 
+;dans les modes majuscule et minuscule.
+;---------------------------------------
+dbeline 
+        .block
+        php         ;Sauve registres
+        pha
+        lda #vline  ;Ligne verticale.
+        sta cleft
+        lda #space  ;Carac espace.
+        sta cmid
+        lda #vline  ;Ligne verticale.
+        sta cright
+        plp         ;Récup registres
+        pla
+        jsr dbdrawline
+        rts
+        .bend
+;---------------------------------------
+;Fonction: dbmline
+;Description:  
+;Fonction qui dessine une ligne 
+;horizontale d'une boite texte en 
+;utilisant les caractères graphiques 
+;disponnibles dans les modes
+;majuscule et minuscule.
+;---------------------------------------
+dbhline        
+        .block
+        php         ;Sauve registres
+        pha
+        lda #hleft  ;Ex. gauche médian
+        sta cleft
+        lda #hline  ;Ligne horizontale
+        sta cmid
+        lda #hright ;Ex. droite médian
+        sta cright
+        plp         ;Récup registres
+        pla
+        jsr dbdrawline
+        rts
+        .bend
+
+;---------------------------------------
+;Fonction: dbdrawline
+;Description:  
+;Fonction qui dessine une ligne 
+;horizontale d'une boite texte en 
+;utilisant les caractères graphiques 
+;disponnibles dans les modes majuscule
+;et minuscule.
+;
+;Note :  
+;Appelé par les fonction suivantes:
+; dbtline, dbbline, dbeline ou dbhline
+;---------------------------------------
+dbdrawline     
+        .block
+        jsr pushregs;Sauve registres
+        lda kcol    ;Sauvegarde de la-
+        sta dbbcoul ;-couleur basic.
+        lda dbcoul  ;Sélect. couleur-     
+        sta kcol    ;-fenêtre.
+        cmp #$10    ;Couleur inverse?
+        bmi norev   ;Non ...
+        lda #18     ;Oui place en inv.
+        jsr chrout 
+norev   ldx dbclin  ;posit curseur au-
+        ldy dbleft  ;-coin supp-gauche-
+        clc         ;-de la fenêtre.
+        jsr plot
+        lda cleft   ;dessine 1er car.
+        jsr chrout
+        lda cmid    ;dessine ligne hor-
+        ldx dbwdth  ;-selon largeur-
+        dex         ;-fenêtre moins-
+        dex         ;-2 car extrémité.
+        jsr putnch
+        lda cright  ;dess. dernier car
+        jsr chrout
+        lda #146    ;fin inverse vidéo
+        jsr chrout
+        lda dbbcoul ;replace la couleur
+        sta kcol    ;basic.
+        inc dbclin;ligne suivante.
+        jsr popregs ;Récup registres
+        rts
+        .bend
+
+;---------------------------------------
+;Fonction: dbdraw
+;Description:  
+;Fonction qui dessine une boite texte  
+;en utilisant les caractères graphiques
+;disponnibles dans les modes
+;majuscule et minuscule.
+;
+;Note :  Appelé par la macro drawbox.
+;        
+;Utilise les fonctions : 
+;dbtline, dbbline, dbeline ou dbhline
+;---------------------------------------
+dbdraw      
+        .block
+        jsr pushregs;Sauve registres
+        ldx dbhght  ;recup hautr boite
+        jsr dbtline ;Dessine ligne sup
+        dex         ;-2 pour les ...
+        dex         ;lignes supp/inf
+        beq linfin  ;Si 0 ligne vide.
+boucle  jsr dbeline ;Dessine ligne vide
+        dex         ;
+        bne boucle  ;prochaine si reste
+linfin  jsr dbbline ;Dessine ligne inf.
+        jsr popregs ;Récup registres
+        rts
+        .bend
+;---------------------------------------
+;Variables globales des paramètres de 
+;dessin de boite texte.
+;---------------------------------------
+cleft   .byte   0   ;Carac Gauche lin 
+cright  .byte   0   ;Carac droit lin
+cmid    .byte   0   ;Carac centr lin
+dbtop   .byte   0   ;Coord haut boite.
+dbleft  .byte   0   ;Coord gauche boîte
+dbwdth  .byte   0   ;Larg de la boîte.
+dbhght  .byte   0   ;Haut de la boîte.
+dbcoul  .byte   0   ;Coul de la boîte.
+dbclin  .byte   0   ;No ligne courante
+dbbcoul .byte   0   ;couleur Basic.   
