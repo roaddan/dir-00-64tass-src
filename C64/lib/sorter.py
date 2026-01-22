08c731cf9e1b7d1da2251edaa6b2a076edd835d8
Lafrance Daniel <roaddan@gmail.com>
2026-01-22 09:48:16 -0500

    20260122-094816

--- C64/lib/sorter.py
+++ C64/lib/sorter.py
@@ -4,8 +4,8 @@ import os
 def main():
     basedir="/Users/Locals/CBM/00-usbkey32go/d-00-64tass-src"
     c64lib="/C64/lib"
-    src=basedir + c64lib + "/e-rom9000.asm"
-    sortsrc(src)
+    for src in {basedir + c64lib + "/e-rom9000.asm", basedir + c64lib + "/e-romc000.asm"}:
+        sortsrc(src)
 
 
 def sortsrc(srcf):
@@ -17,6 +17,10 @@ def sortsrc(srcf):
     new_items=[]
     for item in items:
         equs=item.strip().replace(" ","").split(":")[3].split("=")
+        lib=item.strip().replace(" ","").split(":")[0].replace(".asm","").replace("-c64","")
+        lib=lib.replace("sid-notes-ntsc","notes")
+        lib=lib.replace("9000","")
+        lib=lib.replace("c000","")
         nom=equs[0]
         val=equs[1]
         if val.startswith("$"):
@@ -25,7 +29,7 @@ def sortsrc(srcf):
         else:
             dec_val=val.strip()
             hex_val=hex(int(val)).replace("0x","")
-        new_items.append("%-16s= %5s;%6s" % (nom, "$"+hex_val, dec_val))
+        new_items.append("%-14s=%5s;%5s;%s" % (nom, "$"+hex_val, dec_val,lib))
     sorted_num = sorted(new_items, key=lambda line: int(line.split(';')[1].strip()))
     sorted_alpha = sorted(new_items, key=lambda line: line.split(';')[0].strip())
     fnum =  fpath + fname.replace("-","-c64-").replace(".","-num.")
