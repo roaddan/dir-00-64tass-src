9ec651dd9c4a7ed9f2f6f45291b179e22c90ef70
Lafrance Daniel <roaddan@gmail.com>
2026-01-26 13:15:23 -0500

    20260126-131523

--- V20/sources/screenfmt/screenfmt.asm
+++ V20/sources/screenfmt/screenfmt.asm
@@ -6,18 +6,38 @@ Version = "20260123-101151b"
 ;-----------------------------------------------------------
 main           .block
                #scrcolors vocean, vbleu, vblanc
+               #print ligne
                rts
                .bend
+regdemo        .block
 
- 
+               .bend
+
+text0          .byte $0d
+               .text " CPU REGISTERS HEX "
+               .byte 0
+text1          .byte $0d
+               .text "  pc  ra rx ry sr sp "
+               .byte 0
+text2          .byte $0d
+               .text " 0000 00 00 00 00 00 "
+               .byte 0
+text3          .byte $0d
+               .text " "
+               .byte 0
+ligne          .byte 192,192,192,192,192,192
+               .byte 192,192,192,192,192,192
+               .byte 192,192,192,192,192,192
+               .byte 0               
 
 ;-----------------------------------------------------------
-;     .include  "l-v20-push.asm" 
-;     .include  "l-v20-string.asm" 
-;     .include  "l-v20-mem.asm"           
+     .include  "l-v20-push.asm" 
+     .include  "l-v20-string.asm" 
+     .include  "l-v20-mem.asm"           
 ;     .include  "l-v20-math.asm"           
 ;     .include  "l-v20-conv.asm" 
-;     .include  "l-v20-keyb.asm"         
+;     .include  "l-v20-keyb.asm" 
+     .include  "e-v20-page0.asm"
      .include  "e-v20-vars.asm"
      .include  "m-v20-utils.asm"
      .include  "e-v20-float.asm"
