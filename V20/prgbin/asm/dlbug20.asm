3200bd53ba862c3fba9ed8dba687225d5c20528e
Lafrance Daniel <roaddan@gmail.com>
2026-01-29 21:20:28 -0500

    20260129-212028

--- V20/prgbin/asm/dlbug20.asm
+++ V20/prgbin/asm/dlbug20.asm
@@ -1,7 +1,7 @@
 ;-----------------------------------------------------------
 ; Fichier : keyfinder.asm
 ; Auteur..: Daniel Lafrance
-Version = "20260123-101151"
+Version = "20260123-101151a"
 ;--------------------------------------
 .enc "none"
      .include  "l-v20-bashead-ex.asm"
