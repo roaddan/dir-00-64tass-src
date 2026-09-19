c9176d23795f3f0732714101f61696ec522dc229
Lafrance Daniel <roaddan@gmail.com>
2026-09-19 15:23:38 -0400

    Mis à jour par D.L.: 20260919-152338.

--- V20/liblink.sh
+++ V20/liblink.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
-cd /Users/Locals/CBM/00-usbkey32go/d-00-64tass-src/C64/sources
+export C64SRCDIR="/Users/Locals/CBM/00-usbkey32go/dir-00-64tass-src/C64/sources"
+export C64LIBDIR="/Users/Locals/CBM/00-usbkey32go/dir-00-64tass-src/C64/lib"
+export V20SRCDIR="/Users/Locals/CBM/00-usbkey32go/dir-00-64tass-src/V20/sources"
+export V20LIBDIR="/Users/Locals/CBM/00-usbkey32go/dir-00-64tass-src/V20/lib"
+cd ${C64SRCDIR}
 for I in $(find -type l -iname "lib")
 do
      echo ${I}
      rm ${I}
-     ln -s /Users/Locals/CBM/00-usbkey32go/d-00-64tass-src/C64/lib ${I}
+     ln -s ${C64LIBDIR} ${I}
 done
-cd /Users/Locals/CBM/00-usbkey32go/d-00-64tass-src/V20/sources
+cd ${V20SRCDIR}
 for I in $(find -type l -iname "lib")
 do
      echo ${I}
      rm ${I}
-     ln -s /Users/Locals/CBM/00-usbkey32go/d-00-64tass-src/V20/lib ${I}
+     ln -s ${V20LIBDIR} ${I}
 done
